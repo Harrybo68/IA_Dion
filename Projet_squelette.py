e645e00c0d2e27d59fbe85c30229dd7f6a080caa
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2024-12-02 16:29:30 +0100

    fix3

--- Projet_squelette.py
+++ Projet_squelette.py
@@ -4,7 +4,7 @@ import numpy as np
 import random as rnd
 from threading import Thread
 from queue import Queue
-
+from alphabeta import *
 
 disk_color = ['white', 'red', 'orange']
 disks = list()
@@ -12,71 +12,6 @@ disks = list()
 player_type = ['human']
 for i in range(42):
     player_type.append('AI: alpha-beta level '+str(i+1))
-
-def alpha_beta_decision(board, turn, ai_level, queue, max_player):
-    node_count = 0
-    alpha = -np.inf
-    beta = np.inf
-    best_score = -np.inf
-    best_move = None
-    depth = 0
-
-    for move in board.get_possible_moves():
-        new_board = board.copy()
-        new_board.add_disk(move, ( max_player + 1) % 2, update_display=False)
-        score, node_count = min_value_ab(new_board, turn + 1, ai_level, alpha, beta, node_count, max_player, depth)
-
-        if score > best_score:
-            best_score = score
-            best_move = move
-
-    print(f"Nodes evaluated for best move for alphabeta : {node_count} and best score : {best_score} for player : {max_player}")
-
-    queue.put(best_move)
-    return best_move
-
-def min_value_ab(board, turn, ai_level, alpha, beta, node_count, max_player, depth):
-    node_count += 1
-
-    if board.check_victory():
-        return 1000 - depth, node_count
-    elif depth == ai_level or not board.get_possible_moves():
-        return board.eval(max_player) - depth, node_count
-
-    v = np.inf
-    for move in board.get_possible_moves():
-        new_board = board.copy()
-        new_board.add_disk(move,  ( max_player + 1) % 2 , update_display=False)
-        max_val, node_count = max_value_ab(new_board, turn + 1, ai_level, alpha, beta, node_count, max_player, depth + 1)
-        v = min(v, max_val)
-
-        if v <= alpha:
-            return v, node_count
-        beta = min(beta, v)
-
-    return v, node_count
-
-def max_value_ab(board, turn, ai_level, alpha, beta, node_count, max_player, depth):
-    node_count += 1
-
-    if board.check_victory():
-        return -1000 + depth, node_count
-    elif depth == ai_level or not board.get_possible_moves():
-        return -board.eval(max_player) + depth, node_count
-
-    v = -np.inf
-    for move in board.get_possible_moves():
-        new_board = board.copy()
-        new_board.add_disk(move, max_player, update_display=False)
-        min_val, node_count = min_value_ab(new_board, turn + 1, ai_level, alpha, beta, node_count, max_player, depth + 1)
-        v = max(v, min_val)
-
-        if v >= beta:
-            return v, node_count
-        alpha = max(alpha, v)
-
-    return v, node_count
-
 
 class Board:
     grid = np.array([[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0],
