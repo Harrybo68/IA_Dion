b7fa0f52734a175cf66dc4b85bab4676ab576d8a
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2025-01-23 11:33:54 +0100

    test

--- alphabeta_basic.py
+++ alphabeta_basic.py
@@ -11,7 +11,9 @@ def alpha_beta_decision_basic(board, turn, ai_level, queue, max_player):
     best_move = None
     depth = 0
 
-    for move in board.get_possible_moves():
+    possible_moves = board.get_possible_moves()
+
+    for move in possible_moves:
         new_board = board.copy()
         new_board.add_disk(move, max_player, update_display=False)
         score, node_count = min_value_ab(new_board, turn + 1, ai_level, alpha, beta, node_count, max_player, depth + 1)
@@ -28,18 +30,18 @@ def alpha_beta_decision_basic(board, turn, ai_level, queue, max_player):
 def min_value_ab(board, turn, ai_level, alpha, beta, node_count, max_player, depth):
     node_count += 1
 
-    if board.check_victory():
-        return winner_value, node_count
-    elif not board.get_possible_moves():
-        return draw_value, node_count
-    elif depth == ai_level:
-        #return board.eval(max_player) - depth, node_count
+    possible_moves = board.get_possible_moves()
 
-        return  board.mock_eval(max_player), node_count
+    if board.check_victory():
+        return winner_value - depth, node_count
+    elif not possible_moves:
+        return draw_value - depth, node_count
+    elif depth == ai_level:
+        return  board.mock_eval(max_player) - depth, node_count
 
 
     v = np.inf
-    for move in board.get_possible_moves():
+    for move in possible_moves:
         new_board = board.copy()
         new_board.add_disk(move,  max_player % 2 + 1, update_display=False)
         max_val, node_count = max_value_ab(new_board, turn + 1, ai_level, alpha, beta, node_count, max_player, depth + 1)
@@ -54,16 +56,17 @@ def min_value_ab(board, turn, ai_level, alpha, beta, node_count, max_player, dep
 def max_value_ab(board, turn, ai_level, alpha, beta, node_count, max_player, depth):
     node_count += 1
 
+    possible_moves = board.get_possible_moves()
+
     if board.check_victory():
-        return - winner_value, node_count
-    elif not board.get_possible_moves():
-        return draw_value, node_count
+        return - winner_value + depth, node_count
+    elif not possible_moves:
+        return draw_value + depth, node_count
     elif depth == ai_level:
-        # return board.eval(max_player) + depth, node_count
-        return - board.mock_eval(max_player), node_count
+        return - board.mock_eval(max_player) + depth, node_count
 
     v = -np.inf
-    for move in board.get_possible_moves():
+    for move in possible_moves:
         new_board = board.copy()
         new_board.add_disk(move, max_player, update_display=False)
         min_val, node_count = min_value_ab(new_board, turn + 1, ai_level, alpha, beta, node_count, max_player, depth + 1)
